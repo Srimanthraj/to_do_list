0eebfe77c5f98461cbb7a25ce6d5e9c5c3492915
Srimanthraj <145920613+Srimanthraj@users.noreply.github.com>
2023-10-17 15:36:56 +0530

    Update test.py

--- home/test.py
+++ home/test.py
@@ -1,12 +1,16 @@
 from django.test import TestCase
 from rest_framework import status
 
+from .models import Task
+
 
 class FirstTestCase(TestCase):
     def setUp(self):
-        print("setup called")
+        Task.objects.create(taskTitle='heo', taskDesc='df')
 
-    def test_equal(self):
-        self.assertEqual(2, 2)
-        response = self.client.get('/task2/')
-        self.assertEqual(response.data, {'taskTitle': 'heo', 'taskDesc': 'df'})
+    def test_api_response(self):
+        api_response = self.client.get('/task3/')
+        task = Task.objects.get(taskTitle='heo', taskDesc='df')
+        print("got")
+        self.assertEqual(api_response.data, {
+                         'taskTitle': task.taskTitle, 'taskDesc': task.taskDesc})
